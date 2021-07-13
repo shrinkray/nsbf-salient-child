e0691b6f79c0910e81fb46a05dbc90cd5c1fd7a9
Greg Miller <hello@gregmiller.io>
2021-07-13 12:12:45 -0400

    Add dev dependencies * PostCss, BrowserSync, Clean Webpack Plugin * watch directories

---
+++ webpack.mix.js
@@ -0,0 +1,43 @@
+// webpack.mix.js
+// Jul-13-2021 Shrinkray
+
+let mix = require('laravel-mix');
+ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+mix.js('js/custom-scripts.js', 'dist').setPublicPath('dist')
+    .postCss('css/child-responsive-styles.css', 'dist')
+    .postCss('css/child-styles.css', 'dist')
+    .postCss('css/byways-custom.css', 'dist')
+    .options({
+        postCss :[
+            require('postcss-custom-properties'),
+            require('autoprefixer')
+        ],
+        autoprefixer: {remove: false},
+    })
+    .sourceMaps()
+    .browserSync({proxy: 'http://localhost:10048/'})
+    .webpackConfig({
+            plugins: [
+                new CleanWebpackPlugin({
+                    // Simulate the removal of files
+                    // default: false
+                    dry: true,
+
+                    // Write Logs to Console
+                    // (Always enabled when dry is true)
+                    // default: false
+                    verbose: true,
+
+                    // Automatically remove all unused webpack assets on rebuild
+                    // default: true
+                    cleanStaleWebpackAssets: true,
+
+                    // Do not allow removal of current webpack assets
+                    // default: true
+                    protectWebpackAssets: true,
+                    cleanOnceBeforeBuildPatterns: ['dist/*', '!static-files*'],
+                }),
+            ]
+        }
+    );
