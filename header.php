3494b52280d4b39420e82845ed69a19fbdbc0261
Shrinkray <hello@gregmiller.io>
2022-08-19 11:28:04 -0400

    Add test for value of site_url. If it does not match production, do not add google analytics scripts

--- header.php
+++ header.php
@@ -25,14 +25,24 @@
 	?>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z6NECT3EPH"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+	
+	<?php
+		// if site url is target domain then add tag manager otherwise ignore
+		if ( site_url() === 'https://nsbfoundation.com' ) : ?>
 
-  gtag('config', 'G-Z6NECT3EPH');
-</script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z6NECT3EPH"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-Z6NECT3EPH');
+                </script>
+		<?php
+        endif;
+	?>
+
+
     <title></title>
 
 </head>
