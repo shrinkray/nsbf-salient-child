4d38cbcbf37b83f13aae67d725fb977158afbace
Greg Miller <hello@gregmiller.io>
2024-04-15 14:14:40 -0400

    Update functions and scripts
    - Set enqueued styles
    - Update prettier formatting
    - Add forEach loop to custom-scripts
    - Adds classnames to secondary menu

--- functions.php
+++ functions.php
@@ -58,11 +58,11 @@ function child_theme_styles() {
 	$theme_version = wp_get_theme()->get('Version');
 
     wp_enqueue_style( 'child-styles', 
-		get_stylesheet_directory_uri() . '/dist/child-styles.min.css',
+		get_stylesheet_directory_uri() . '/dist/child-styles.css',
       [], $theme_version, 'all' );
     
 		wp_enqueue_style( 'child-responsive-styles', 
-		get_stylesheet_directory_uri() . '/dist/child-responsive-styles.min.css',
+		get_stylesheet_directory_uri() . '/dist/child-responsive-styles.css',
 			[], $theme_version, 'all' );
 		
 		wp_enqueue_style( 'byway-styles', 
@@ -74,6 +74,9 @@ add_action( 'wp_enqueue_scripts', 'child_theme_styles', 999, 1 );
 //Enqueue Scripts from child theme
 
 function child_theme_scripts() {
+
+	$theme_version = wp_get_theme()->get('Version');
+
     wp_enqueue_script( 'custom-scripts', 
 		get_stylesheet_directory_uri() . '/dist/custom-scripts.js', array('jquery'),
 	    $theme_version,
--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,45 +1,45 @@
 jQuery(document).ready(function($){
-
     /* Add random images functionality to slider */
 
-    var sliderLength = $('.randomize-slides .n2-ss-slide-backgrounds > div').length;
-    var randomSlider = Math.floor((Math.random() * sliderLength));
-    var sliderTransformStyle = 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -100000, 0, 0, 1)';
+    var sliderLength = $(
+        '.randomize-slides .n2-ss-slide-backgrounds > div'
+    ).length;
+    var randomSlider = Math.floor(Math.random() * sliderLength);
+    var sliderTransformStyle =
+        'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -100000, 0, 0, 1)';
 
-    $('.randomize-slides .n2-ss-slide-backgrounds > div').each(function(){
+    $('.randomize-slides .n2-ss-slide-backgrounds > div').each(function () {
         $(this).css('transform', sliderTransformStyle);
-    });    
-    
+    });
+
     console.log(randomSlider);
-    
-    jQuery(window).on("load",function(){        
 
-        setTimeout(function(){
-            $('.randomize-slides .n2-ss-slide-backgrounds > div').eq(randomSlider).css('transform', 'initial');
+    jQuery(window).on('load', function () {
+        setTimeout(function () {
+            $('.randomize-slides .n2-ss-slide-backgrounds > div')
+                .eq(randomSlider)
+                .css('transform', 'initial');
         }, 1000);
-
     });
 
     /* End */
-    
+
     /* Script to hide all newsletters except for the latest year */
 
-    var firstIteration = true
+    var firstIteration = true;
 
-    $('.newsletter-inner > .nsbf-newsletter').each(function(){
-
-        if(firstIteration){
+    $('.newsletter-inner > .nsbf-newsletter').each(function () {
+        if (firstIteration) {
             firstIteration = false;
-        } else{
+        } else {
             $(this).hide();
         }
     });
 
     /* Show newsletters by the year that is clicked */
-    
-    $('.newsletter-inner .newsletter-header').click(function(){
 
-        $('.nsbf-newsletter').hide();        
+    $('.newsletter-inner .newsletter-header').click(function () {
+        $('.nsbf-newsletter').hide();
         $('.newsletter-header').css('background', '#f0f0f0');
         $('.newsletter-header > *').css('color', '#006b94');
 
@@ -47,19 +47,67 @@ jQuery(document).ready(function($){
         $(this).css('background', '#dba510');
         $('*', this).css('color', '#fff');
 
-        $('html, body').animate({
-            scrollTop: ($(this).offset().top - 130)
-        }, 1000);
+        $('html, body').animate(
+            {
+                scrollTop: $(this).offset().top - 130,
+            },
+            1000
+        );
     });
 
     /* End */
 
     /* Show error message of MemberPress if user "Set Password" has expired */
 
-    if( $('#mepr_jump')[0] && $('.mepr_error')[0] ){
+    if ($('#mepr_jump')[0] && $('.mepr_error')[0]) {
         $('#link-expired').show();
     }
 
     /* End */
 
+    /**
+     * Secondary menu is assigning colors to tab backgrounds
+     * The next two scripts addresses the issue
+     * 
+     * First adds a classname to the last item
+     * Second resets the color pattern for all the secondary buttons
+     */
+
+    // Select the last <li> element within the specified parent element
+    var lastLi = document.querySelector(
+        '#header-secondary-outer nav ul.sf-menu li:last-child'
+    );
+
+    // Add the required classes to the last <li> element
+    lastLi.classList.add('secondary-nav-button');
+
+    /**
+     * Resets colors for each of the buttons
+     * Using ForEach loop
+     */
+    // Define the array of classes to cycle through
+    var buttonClasses = [
+        'gold-button',
+        'rust-button',
+        'green-button',
+        'blue-button',
+    ];
+
+    // Select all <li> elements within the specified parent
+    var lis = document.querySelectorAll(
+        '#header-secondary-outer nav ul.sf-menu li'
+    );
+
+    // Loop through the <li> elements and assign classes from the array in order
+    lis.forEach(function (li, index) {
+        // Remove any existing color-button classes
+        buttonClasses.forEach(function (className) {
+            li.classList.remove(className);
+        });
+
+        // Add the next class in the array, cycling back to the start if necessary
+        var classToAdd =
+            buttonClasses[index % buttonClasses.length];
+        li.classList.add(classToAdd);
+    });
 });
