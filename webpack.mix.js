e7304cef2c17c3b367b6fe5988ae7a65bca2deff
Greg Miller <hello@gregmiller.io>
2024-05-08 16:38:46 -0400

    Update configs
    - prettier fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,51 +1,47 @@
 // webpack.mix.js
 // April 2024 -- Shrinkray
 
-const mix = require("laravel-mix");
-const tailwindcss = require("tailwindcss");
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 let autoprefixer;
-autoprefixer = require("autoprefixer");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const fs = require("fs");
-const path = require("path");
+autoprefixer = require('autoprefixer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const fs = require('fs');
+const path = require('path');
 
-mix
-  .js("js/custom-scripts.js", "dist")
-  .sass("scss/byways.scss", "dist", {
-    implementation: require("sass"),
-  })
-  .options({
-    processCssUrls: false,
-    postCss: [tailwindcss(), require("autoprefixer")],
-  })
-  // You can chain the .css() here as you need
-  .css("css/child-responsive-styles.css", "dist")
-  .css("css/child-styles.css", "dist")
+mix.js('js/custom-scripts.js', 'dist')
+    .sass('scss/byways.scss', 'dist', {
+        implementation: require('sass'),
+    })
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss(), require('autoprefixer')],
+    })
+    // You can chain the .css() here as you need
+    .css('css/child-responsive-styles.css', 'dist')
+    .css('css/child-styles.css', 'dist')
 
-  // merge the original stylesheets
-  .styles(
-    ["dist/child-responsive-styles.css", "dist/child-styles.css"],
-    "dist/merged-styles.css",
-)
-  
+    // merge the original stylesheets
+    .styles(
+        ['dist/child-responsive-styles.css', 'dist/child-styles.css'],
+        'dist/merged-styles.css',
+    )
 
-  // Add more configurations if necessary
-  .sourceMaps(!mix.inProduction())
+    // Add more configurations if necessary
+    .sourceMaps(!mix.inProduction())
 
-  
-
-  .webpackConfig({
-    plugins: [
-      new CleanWebpackPlugin({
-        // Simulate the removal of files
-        dry: false,
-        // Write Logs to Console
-        verbose: false,
-        // Automatically remove all unused webpack assets on rebuild
-        cleanStaleWebpackAssets: true,
-        // Do not allow removal of current webpack assets
-        protectWebpackAssets: true,
-        cleanOnceBeforeBuildPatterns: ["dist/*", "!static-files*"],
-      }),
-    ],
-  });
+    .webpackConfig({
+        plugins: [
+            new CleanWebpackPlugin({
+                // Simulate the removal of files
+                dry: false,
+                // Write Logs to Console
+                verbose: false,
+                // Automatically remove all unused webpack assets on rebuild
+                cleanStaleWebpackAssets: true,
+                // Do not allow removal of current webpack assets
+                protectWebpackAssets: true,
+                cleanOnceBeforeBuildPatterns: ['dist/*', '!static-files*'],
+            }),
+        ],
+    });
