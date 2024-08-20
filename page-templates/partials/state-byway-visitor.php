0e927a8a988d08f43004c186da3bbf1014c4d8ec
Greg Miller <hello@gregmiller.io>
2024-08-20 17:36:58 -0400

    Update state-byway-visitor.php

--- page-templates/partials/state-byway-visitor.php
+++ page-templates/partials/state-byway-visitor.php
@@ -19,36 +19,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="label-minor-heading">Statewide Byway Partners</div>
 
-<div class="departments grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-2">
+<div class="grid grid-cols-1 gap-2 departments sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
 
-	<div class="partner-digits">
-		
-		<div class="detail-organization"><?php echo $state_dot_name; ?></div>
+    <div class="partner-digits">
+        
+        <div class="detail-organization"><?php echo esc_html( $state_dot_name ); ?></div>
 
-		<div class="detail-properties">
-			<?php
-			// If we have a website URL add a link
-			if ( $state_dot_byway_website ) :
-				?>
-					<a class="byway-website-property" href="
-					<?php
-					echo $state_dot_byway_website;
-					?>
-					" target="_blank" title="Learn more at the <?php echo $state_dot_name; ?> website!">Website</a>
+        <div class="detail-properties">
+			<?php // If we have a website URL add a link
+				if ( $state_dot_byway_website ) : ?>
+                    <a class="byway-website-property" href="<?php echo esc_url( $state_dot_byway_website );
+					?>" target="_blank" title="Learn more at the <?php echo esc_attr( $state_dot_name ); ?> website!">Website</a>
 				<?php
 
 				endif;
 			?>
 			
-			<?php
-			// If we have a phone URL add a link
-			if ( $state_dot_byway_phone ) :
-				?>
-					<a class="byway-phone-property" href="tel:
-					<?php
-					echo $state_dot_byway_phone;
-					?>
-					" title="Need help? Call our offices."><?php echo $state_dot_byway_phone; ?></a>
+			<?php // If we have a phone URL add a link
+				if ( $state_dot_byway_phone ) : ?>
+                    <a class="byway-phone-property" href="tel:<?php echo esc_url( $state_dot_byway_phone );
+					?>" title="Need help? Call our offices."><?php echo esc_attr( $state_dot_byway_phone ); ?></a>
 				<?php
 
 				endif;
@@ -66,29 +56,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 		if ( $state_tourism_board_name ) :
 			?>
 
-		<div class="detail-organization"><?php echo $state_tourism_board_name; ?></div>
+        <div class="detail-organization"><?php echo esc_html( $state_tourism_board_name );?></div>
 
-		<div class="detail-properties">
-			<?php
-			// If we have a website URL add a link
-			if ( $state_tourism_board_website ) :
-				?>
-					<a class="byway-website-property" href="
-					<?php
-					echo $state_tourism_board_website;
-					?>
-					" target="_blank" title="Learn more at the <?php echo $state_tourism_board_name; ?> website!">Website</a>
+        <div class="detail-properties">
+			<?php // If we have a website URL add a link
+				if ( $state_tourism_board_website ) :  ?>
+                    <a class="byway-website-property" href="<?php echo esc_url( $state_tourism_board_website );
+					?>" target="_blank" title="Learn more at the <?php echo esc_attr( $state_tourism_board_name ); ?> website!">Website</a>
 				<?php endif; ?>
 			
-			<?php
-			// If we have a phone URL add a link
-			if ( $state_tourism_board_phone ) :
-				?>
-					<a class="byway-phone-property" href="tel:
-					<?php
-					echo $state_tourism_board_phone;
-					?>
-					" title="Need help? Call our offices."><?php echo $state_tourism_board_phone; ?></a>
+			<?php // If we have a phone URL add a link
+				if (  $state_tourism_board_phone ) :  ?>
+                    <a class="byway-phone-property" href="tel:<?php echo esc_url( $state_tourism_board_phone );
+					?>" title="Need help? Call our offices."><?php echo esc_attr( $state_tourism_board_phone );?></a>
 				<?php endif; ?>
 		</div> <!-- .detail-properties -->
 		<?php endif; ?>
