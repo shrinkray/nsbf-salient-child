ad9dd935d92503253878025f1686bc32649ae266
Greg Miller <hello@gregmiller.io>
2024-08-20 17:49:35 -0400

    Update byway-detail.php

--- page-templates/partials/byway-detail.php
+++ page-templates/partials/byway-detail.php
@@ -1,18 +1,19 @@
 <?php
 	/**
-	 * @template Details Section
-	 * @date Jul142021
-	 * @author Greg Miller, gregmiller.io
+	 * @template   Details Section
+	 * @date       Jul142021
+	 * @date       Aug7,2024
+	 * @author     Greg Miller, gregmiller.io
 	 * @testedwith
 	 */
-	// Exit if accessed directly
+	// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
 
-<div class="row grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mb-12">
-	<div class="section details order-last md:order-none lg:order-none ">
+<div class="grid grid-cols-1 mb-12 row md:grid-cols-2 lg:grid-cols-2">
+	<div class="order-last section details md:order-none lg:order-none">
 		<div id="details" class="anchored"></div>
 		<h2 class="text-3xl md:text-4xl h2 wayfinder">Details</h2>
 		<?php
@@ -23,12 +24,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 			$designation_year                   = get_field( 'nb_designation_year' );
 			$length_of_byway_miles              = get_field( 'nb_length_of_byway_miles' );
 
-			// split the phrase by any number of commas or space characters into an array()
-			// which include " ", \r, \t, \n and \f
+			// split the phrase by any number of commas or space characters into an array().
+			// which include " ", \r, \t, \n and \f.
 
 			$keywords = preg_split( '/[\s,]+/', $intrinsic_quality );
 
-			// for each single letter associate a word and build a new string
+			// for each single letter associate a word and build a new string.
 		foreach ( $keywords as $keyword ) {
 
 			switch ( $keyword ) {
@@ -56,163 +57,196 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 			$typelist .= $quality . ', ';
 		}
-			// Remove space and last comma from the list and return the trimmed result
+			// Remove space and last comma from the list and return the trimmed result.
 			$trimmed = rtrim( trim( $typelist ), ',' );
 
 		// echo $list;
 		?>
 		<ul class="detail-list mt-7 mb-7">
-			<li><span class="label-minor-heading">Designation</span><?php echo $designation; ?> (<?php echo $designation_year; ?>)</li>
+			<li><span class="label-minor-heading">Designation info</span>
+			<?php
+			echo esc_html( $designation );
+			?>
+			(<?php echo esc_html( $designation_year ); ?>)</li>
 			<li><span class="label-minor-heading">Intrinsic Qualities</span>
 			<?php
-			echo $trimmed;
+			echo esc_html( $trimmed );
 			?>
-				</li>
+			</li>
 			<li><span class="label-minor-heading">Location</span>
 			<?php
-			echo $state_or_states_that_contain_byway;
+			echo esc_html( $state_or_states_that_contain_byway );
 			?>
-				</li>
-			<li><span class="label-minor-heading">Length</span><?php echo $length_of_byway_miles; ?>&nbsp;miles</li>
+			</li>
+			<li><span class="label-minor-heading">Length</span>
+			<?php
+			echo esc_html( $length_of_byway_miles );
+			?>
+			&nbsp;miles</li>
 		</ul>
 		
 		
 		<?php
-			// vars
-			$dedicated_byway_organization         = get_field( 'nb_dedicated_byway_organization' );
-			$dedicated_byway_organization_website = get_field( 'nb_dedicated_byway_organization_website' );
-			$dedicated_byway_organization_phone   = get_field( 'nb_dedicated_byway_organization_phone' );
+		// vars
+		$dedicated_byway_organization         = get_field( 'nb_dedicated_byway_organization' );
+		$dedicated_byway_organization_website = get_field( 'nb_dedicated_byway_organization_website' );
+		$dedicated_byway_organization_phone   = get_field( 'nb_dedicated_byway_organization_phone' );
 
 		?>
 	<?php
-		// Add if we have a field for a dedicated organization
+		// Add if we have a field for a dedicated organization.
 	if ( $dedicated_byway_organization ) :
 
 		?>
 		<!--                Dedicated organization -->
 		<div class="detail-subsection">
 			<div class="label-minor-heading">Byway Visitor Information</div>
-			<div class="detail-organization"><?php echo $dedicated_byway_organization; ?></div>
+			<div class="detail-organization"><?php echo esc_html( $dedicated_byway_organization ); ?></div>
 			<div class="detail-properties">
 				
-			<?php
-				// If we have a website URL add a link
-			if ( $dedicated_byway_organization_website ) :
-				?>
+		<?php
+		// If we have a website URL add a link.
+		if ( $dedicated_byway_organization_website ) :
+			?>
 						<a class="byway-website-property" href="
 						<?php
-						echo $dedicated_byway_organization_website;
+						echo esc_url( $dedicated_byway_organization_website );
 						?>
-						" target="_blank" title="Learn more at the <?php echo $dedicated_byway_organization; ?> website!">Website</a>
-					<?php endif; ?>
+						" target="_blank" title="Learn more at the 
+						<?php
+						echo esc_attr( $dedicated_byway_organization );
+						?>
+						website!">Website</a>
+		<?php endif; ?>
 				
-				<?php
-				// If we have a phone URL add a link
-				if ( $dedicated_byway_organization_phone ) :
-					?>
+		<?php
+		// If we have a phone URL add a link.
+		if ( $dedicated_byway_organization_phone ) :
+			?>
 						<a class="byway-phone-property" href="tel:
 						<?php
-						echo $dedicated_byway_organization_phone;
+						echo esc_url( $dedicated_byway_organization_phone );
 						?>
-						" title="Need help? Call our offices."><?php echo $dedicated_byway_organization_phone; ?></a>
-					<?php endif; ?>
+						" title="Need help? Call our offices.">
+						<?php
+						echo esc_attr( $dedicated_byway_organization_phone );
+						?>
+						</a>
+		<?php endif; ?>
 
 			</div> <!-- .detail-organization -->
 
 		</div> <!-- .detail-subsection -->
 		<?php
-	endif; // dedicated organization
+	endif; // dedicated organization.
 	?>
 
 
 		<div class="detail-subsection mt-7">
 			<div class="label-minor-heading">Statewide Byway Partners</div>
 			
-			<div class="departments grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-2">
+			<div class="grid grid-cols-1 gap-2 departments sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
 				<div class="partner-digits">
 					<?php
-						// vars
-						$state_dot_name          = get_field( 'nb_state_dot_name' );
-						$state_dot_byway_website = get_field( 'nb_state_dot_byway_website' );
-						$state_dot_byway_phone   = get_field( 'nb_state_dot_byway_phone' );
+					// vars
+					$state_dot_name          = get_field( 'nb_state_dot_name' );
+					$state_dot_byway_website = get_field( 'nb_state_dot_byway_website' );
+					$state_dot_byway_phone   = get_field( 'nb_state_dot_byway_phone' );
 
-						/**
-						 * If the organization exists, add it's name. If the web and or phone properties exist,
-						 * render, otherwise do not show them.
-						 */
+					/**
+					 * If the organization exists, add it's name. If the web and or phone properties exist,
+					 * render, otherwise do not show them.
+					 */
 					if ( $state_dot_name ) :
 						?>
-							<div class="detail-organization"><?php echo $state_dot_name; ?></div>
+							<div class="detail-organization"><?php echo esc_html( $state_dot_name ); ?></div>
 
 							<div class="detail-properties">
-								<?php
-								// If we have a website URL add a link
-								if ( $state_dot_byway_website ) :
-									?>
+						<?php
+						// If we have a website URL add a link.
+						if ( $state_dot_byway_website ) :
+							?>
 										<a class="byway-website-property" href="
 										<?php
-										echo $state_dot_byway_website;
+										echo esc_url( $state_dot_byway_website );
 										?>
-										" target="_blank" title="Learn more at the <?php echo $state_dot_name; ?> website!">Website</a>
-									<?php endif; ?>
+										" target="_blank" title="Learn more at the 
+										<?php
+										echo esc_attr( $state_dot_name );
+										?>
+										website!">Website</a>
+						<?php endif; ?>
 								
-								<?php
-								// If we have a phone URL add a link
-								if ( $state_dot_byway_phone ) :
-									?>
+						<?php
+						// If we have a phone URL add a link.
+						if ( $state_dot_byway_phone ) :
+							?>
 										<a class="byway-phone-property" href="tel:
 										<?php
-										echo $state_dot_byway_phone;
+										echo esc_url( $state_dot_byway_phone );
 										?>
-										" title="Need help? Call our offices."><?php echo $state_dot_byway_phone; ?></a>
-									<?php endif; ?>
+										" title="Need help? Call our offices.">
+										<?php
+										echo esc_attr( $state_dot_byway_phone );
+										?>
+										</a>
+						<?php endif; ?>
 							</div> <!-- .detail-properties -->
 						<?php
-						endif; // $state_dot_name
+					endif; // $state_dot_name
 					?>
 				</div> <!-- .partner-digits -->
 
 				<div class="partner-digits">
 					<?php
-						// vars
-						$state_tourism_board_name    = get_field( 'nb_state_tourism_board_name' );
-						$state_tourism_board_website = get_field( 'nb_state_tourism_board_website' );
-						$state_tourism_board_phone   = get_field( 'nb_state_tourism_board_phone' );
+					// vars
+					$state_tourism_board_name    = get_field( 'nb_state_tourism_board_name' );
+					$state_tourism_board_website = get_field( 'nb_state_tourism_board_website' );
+					$state_tourism_board_phone   = get_field( 'nb_state_tourism_board_phone' );
 
-						/**
-						 * If the organization exists, add it's name. If the web and or phone properties exist,
-						 * render, otherwise do not show them.
-						 */
+					/**
+					 * If the organization exists, add it's name.
+					 * If the web and or phone properties exist.
+					 * render, otherwise do not show them.
+					 */
 					if ( $state_tourism_board_name ) :
 						?>
 
-							<div class="detail-organization"><?php echo $state_tourism_board_name; ?></div>
+							<div class="detail-organization"><?php echo esc_html( $state_tourism_board_name ); ?></div>
 
 							<div class="detail-properties">
-								<?php
-								// If we have a website URL add a link
-								if ( $state_tourism_board_website ) :
-									?>
+						<?php
+						// If we have a website URL add a link.
+						if ( $state_tourism_board_website ) :
+							?>
 										<a class="byway-website-property" href="
 										<?php
-										echo $state_tourism_board_website;
+										echo esc_url( $state_tourism_board_website );
 										?>
-										" target="_blank" title="Learn more at the <?php echo $state_tourism_board_name; ?> website!">Website</a>
-									<?php endif; ?>
+										" target="_blank" title="Learn more at the 
+										<?php
+										echo esc_attr( $state_tourism_board_name );
+										?>
+										website!">Website</a>
+						<?php endif; ?>
 								
-								<?php
-								// If we have a phone URL add a link
-								if ( $state_tourism_board_phone ) :
-									?>
+						<?php
+						// If we have a phone URL add a link.
+						if ( $state_tourism_board_phone ) :
+							?>
 										<a class="byway-phone-property" href="tel:
 										<?php
-										echo $state_tourism_board_phone;
+										echo esc_url( $state_tourism_board_phone );
 										?>
-										" title="Need help? Call our offices."><?php echo $state_tourism_board_phone; ?></a>
-									<?php endif; ?>
+										" title="Need help? Call our offices.">
+										<?php
+										echo esc_attr( $state_tourism_board_phone );
+										?>
+										</a>
+						<?php endif; ?>
 							</div> <!-- .detail-properties -->
 						<?php
-						endif; // $state_tourism_board_name
+					endif; // $state_tourism_board_name
 					?>
 				</div> <!-- .partner-digits -->
 			</div> <!-- .departments -->
@@ -222,31 +256,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		</div> <!-- .detail-subsection // Statewide Byway Partners  -->
 	</div> <!-- .section -->
-	<div class="section image order-first mb-8 md:order-none lg:order-none">
+	<div class="order-first mb-8 section image md:order-none lg:order-none">
+		<?php
+		$image = get_the_post_thumbnail(
+			$post_id,
+			'byway_large',
+			array( 'alt' => get_the_title() )
+		);
+
+
+		// checks for post image if none, do not show empty box nor line
+		if ( has_post_thumbnail() ) :
+			?>
 		<div class="detail-image">
 			<?php
 
-				$image = get_the_post_thumbnail( $post_id, 'byway_large' );
-			echo $image;
+				echo acf_esc_html( $image );
+
 			?>
 		</div>
-		<div class="attribution text-right italic">
+			<?php
+		else :
+			?>
+			<div class="detail-image placeholder"></div>
+			<?php
+		endif;
+		?>
+		<div class="italic attribution">
 		 
 			<?php
 			if ( ! empty( have_rows( 'nb_iconic_images' ) ) ) :
 				$first_credit = true;
 
-				// combo conditional to get just the first record
+				// combo conditional to get just the first record.
 				while ( $first_credit && have_rows( 'nb_iconic_images' ) ) :
 					the_row();
 
 					$attribution = get_sub_field( 'image_attribution' );
-					// set  false to stop from getting the next record
-					$first_credit = false;
+					// set  false to stop from getting the next record.
+						$first_credit = false;
 					?>
 					 
 
-				<span class="source"><?php echo $attribution; ?></span>
+				<span class="source"><?php echo acf_esc_html( $attribution ); ?></span>
 					<?php if ( ! empty( $attribution ) ) : ?>
 				<span class="photo-credit"> Photo</span>
 						<?php
@@ -256,7 +308,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 				endwhile;
 				?>
 			<?php else : ?>
-				<?php // no rows found ?>
+				<?php // no rows found. ?>
 			<?php endif; ?>
 		</div>
 	</div> <!-- .section -->
