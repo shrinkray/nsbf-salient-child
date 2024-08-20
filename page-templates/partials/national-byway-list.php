858f8c23f0c725eb253ca271d94f5aa02e856e9d
Greg Miller <hello@gregmiller.io>
2024-08-20 16:12:11 -0400

    Update national-byway-list.php
    - Beautify
    - Update for code standards
    - HTML escaping
    - Move wp_reset_postdata() to parent

--- page-templates/partials/national-byway-list.php
+++ page-templates/partials/national-byway-list.php
@@ -1,75 +1,79 @@
 <?php
-	
+
 	/**
 	 * @template-part national-byway-list
 	 * @date Jul-16-2021
 	 * @author Greg Miller, gregmiller.io
 	 *
-     * @date 9-16-22
-     * Add check if $title is Texas; Return message about Additional Byways
+	 * @date 9-16-22
+	 * Add check if $title is Texas; Return message about Additional Byways
 	 */
 // Exit if accessed directly
-	if ( ! defined( 'ABSPATH' ) ) {
-		exit;
-	}
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 ?>
 
 
 <h2 class="mt-12 mb-8 text-2xl md:text-3xl text-outerspace">America's Byways Collection</h2>
 <!--    <ul class="grid grid-cols-1 byway-collection">-->
-    <ul class="grid grid-cols-1 byway-collection gap-x-4">
+	<ul class="grid grid-cols-1 byway-collection gap-x-4">
 
-    <?php
+	<?php
 		// Loop querying posts for National Byways ($nb_query) to present the Byway Info
-		if ( !empty( $nb_query->have_posts() ) ) :
-			
-			while ( $nb_query->have_posts() ) :
-				$nb_query->the_post();
-				$permalink = get_permalink( $nb_query->ID );
-				$query_id  = get_the_title( $nb_query->ID );
-				
-				// below is replaced by the $terms_string variable, recommended by coPilot
-				// $tax  = get_the_terms( $nb_query->ID, 'nb_designation' )[0]->slug;
-				
-				// Gets the taxonomy slug from the $nb_query array and assigns it to $tax
-				$terms = get_the_terms( get_the_ID(), 'nb_designation' );
-           	 	$tax = !empty($terms) ? $terms[0]->slug : '';
-				
-				// If the tax equates to All American Roadway, we echo out our notification in the list item below
-				if ( $tax === "aar" )  :
-					$asterisk = '*';
+	if ( ! empty( $nb_query->have_posts() ) ) :
+
+		while ( $nb_query->have_posts() ) :
+			$nb_query->the_post();
+			$permalink = get_permalink( $nb_query->ID );
+			$query_id  = get_the_title( $nb_query->ID );
+
+			// below is replaced by the $terms_string variable, recommended by coPilot
+			// $tax  = get_the_terms( $nb_query->ID, 'nb_designation' )[0]->slug;
+
+			// Gets the taxonomy slug from the $nb_query array and assigns it to $tax
+			$terms = get_the_terms( get_the_ID(), 'nb_designation' );
+			$tax   = ! empty( $terms ) ? $terms[0]->slug : '';
+
+			// If the tax equates to All American Roadway, we echo out our notification in the list item below
+			if ( $tax === 'aar' ) :
+				$asterisk = '*';
 				else :
-                    $asterisk = '';
-				endif;	?>
+					$asterisk = '';
+				endif;
+				?>
 	
 				<li class="byway-item">
-					<a href="<?php echo esc_url( $permalink ); ?>" class="item-link"><?php echo esc_html( $query_id ); ?></a><?php echo
-                    esc_html( $asterisk ); ?>
+					<a href="<?php echo esc_url( $permalink ); ?>" class="item-link"><?php echo esc_html( $query_id ); ?></a>
+										<?php
+										echo esc_html( $asterisk );
+										?>
 				</li>
-    
+	
 			<?php
 			endwhile;
-        else : // Instead of creating a list, show this gentle message.
+		else : // Instead of creating a list, show this gentle message.
 			?>
 			<li class="byway-item">
 				Currently there are no All-American Roads or National Scenic Byways in <?php echo esc_html( $title ); ?>.
 			</li>
    
-		<?php // Set this value here instead of on state byway list
-            if ( $title === 'Texas' ) :
-                ?>
-                <h2 class="mt-10 mb-8 text-2xl md:text-3xl text-outerspace">Additional Byways</h2>
-                <div class="unlinked">
-                    <p>Currently there are no additional byways in <?php echo esc_html( $title ); ?></p>
-                </div>
-                
-            <?php
-            // see state-byway-list.php for script triggering visibility of the 'update' link (line 97)
-            endif;
+			<?php
+			// Set this value here instead of on state byway list
+			if ( $title === 'Texas' ) :
+				?>
+				<h2 class="mt-10 mb-8 text-2xl md:text-3xl text-outerspace">Additional Byways</h2>
+				<div class="unlinked">
+					<p>Currently there are no additional byways in <?php echo esc_html( $title ); ?></p>
+				</div>
+				
+				<?php
+				// see state-byway-list.php for script triggering visibility of the 'update' link (line 97)
+			endif;
 		endif;
-	?>
+		?>
 
 </ul> <!-- .byway-collection (National) -->
 
 <?php
-	wp_reset_postdata();
+
