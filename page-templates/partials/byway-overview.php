54eeafceea2b1534dd2469520f01530aa9a50e4e
Greg Miller <hello@gregmiller.io>
2024-08-20 17:42:42 -0400

    Update byway-overview.php

--- page-templates/partials/byway-overview.php
+++ page-templates/partials/byway-overview.php
@@ -12,9 +12,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 	
-	<div class="row mt-3 mb-12"><!-- overview  -->
+	<div class="mt-3 mb-12 row"><!-- overview  -->
 		<div class="section">
-			<div class="boxed-subsection border-2 md:border-4">
+			<div class="border-2 boxed-subsection md:border-4">
 				<?php
 					// vars
 					$byway_synopsis = get_field( 'nb_byway_synopsis' );
@@ -26,12 +26,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 				if ( ! empty( $byway_synopsis ) ) :
 					?>
 				<h2 class="text-3xl md:text-4xl h2 overview">Overview</h2>
-					<?php echo $byway_synopsis; ?>
+					<?php echo acf_esc_html( $byway_synopsis ); ?>
 
 						<script>
-							const itemOverview = document.getElementById('item-overview');
-							itemOverview.classList.remove('hidden');
-							itemOverview.classList.add('block');
+							const itemOverview = document.getElementById( 'item-overview' );
+							itemOverview.classList.remove( 'hidden' );
+							itemOverview.classList.add( 'block' );
 						</script>
 					<?php
 				endif; // end
