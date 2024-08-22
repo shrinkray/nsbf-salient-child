384b519819fbd641486494e02d7156caedc81185
Greg Miller <hello@gregmiller.io>
2024-08-22 16:18:20 -0400

    Update byway-detail-sb.php
    - Fix phpcs bugs

--- page-templates/partials/byway-detail-sb.php
+++ page-templates/partials/byway-detail-sb.php
@@ -1,12 +1,14 @@
 <?php
-	/**
-	 * @template   Details Section
-	 * @date       Jul142021
-	 * @update     Aug072024
-	 * @author     Greg Miller, gregmiller.io
-	 * @testedwith
-	 */
-	// Exit if accessed directly.
+/**
+ * State byway (sb) details page.
+ *
+ * @template   Details Section
+ * @date       Jul142021
+ * @update     Aug072024
+ * @author     Greg Miller, gregmiller.io
+ * @package    Template
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -29,7 +31,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		?>
 		<ul class="detail-list mt-7 mb-7">
 			<!--  Setting Year only if it exists, otherwise do not show parenthesis   -->
-			<li><span class="label-minor-heading">Designation</span><?php echo esc_html( $designation ); ?> <?php
+			<li><span class="label-minor-heading">Designation</span>
+			<?php echo esc_html( $designation ); ?> <?php
 					echo $designation_year ? '(' . esc_html( $designation_year ) . ')' : ''
 			?>
 																				</li>
@@ -39,48 +42,52 @@ if ( ! defined( 'ABSPATH' ) ) {
 			echo esc_html( $sb_state );
 			?>
 			</li>
-			<li><span class="label-minor-heading">Length</span><?php echo esc_html( $length_of_byway_miles ); ?>&nbsp;miles</li>
+			<li><span class="label-minor-heading">Length</span>
+			<?php echo esc_html( $length_of_byway_miles ); ?>&nbsp;miles</li>
 		</ul>
 		
 		
 		<?php
-		// vars
+		// vars.
 		$dedicated_byway_organization         = get_field( 'sb_dedicated_byway_organization' );
-		$dedicated_byway_organization_website = get_field( 'sb_dedicated_byway_organization_website' );
-		$dedicated_byway_organization_phone   = get_field( 'sb_dedicated_byway_organization_phone' );
+		$dedicated_byway_organization_website =
+		get_field( 'sb_dedicated_byway_organization_website' );
+		$dedicated_byway_organization_phone   =
+		get_field( 'sb_dedicated_byway_organization_phone' );
 
-		?>
-		<?php
 		// Add if we have a field for a dedicated organization.
 		if ( $dedicated_byway_organization ) :
 			?>
 		<!--                Dedicated organization -->
 		<div class="detail-subsection">
 			<div class="label-minor-heading">Dedicated Byway Visitor Information</div>
-			<div class="detail-organization"><?php echo esc_html( $dedicated_byway_organization ); ?></div>
+			<div class="detail-organization">
+				<?php echo esc_html( $dedicated_byway_organization ); ?></div>
 			<div class="detail-properties">
 				
 			<?php
 			// If we have a website URL add a link.
 			if ( $dedicated_byway_organization_website ) :
 				?>
-						<a class="byway-website-property" href="
-						<?php
-						echo esc_url( $dedicated_byway_organization_website );
-						?>
-						" target="_blank" title="Learn more at the <?php echo esc_attr( $dedicated_byway_organization ); ?> website!">Website</a>
+				<a class="byway-website-property" href="
+				<?php
+				echo esc_url( $dedicated_byway_organization_website );
+				?>
+				" target="_blank" title="Learn more at the 
+				<?php echo esc_attr( $dedicated_byway_organization ); ?> website!">Website</a>
 			<?php endif; // website. ?>
 				
 			<?php
 			// If we have a phone URL add a link.
 			if ( $dedicated_byway_organization_phone ) :
 				?>
-						<a class="byway-phone-property" href="tel:
-						<?php
-						echo esc_url( $dedicated_byway_organization_phone );
-						?>
-						" title="Need help? Call our offices."><?php echo esc_attr( $dedicated_byway_organization_phone ); ?></a>
-			<?php endif; // phone ?>
+				<a class="byway-phone-property" href="tel:
+				<?php
+				echo esc_url( $dedicated_byway_organization_phone );
+				?>
+				" title="Need help? Call our offices.">
+				<?php echo esc_attr( $dedicated_byway_organization_phone ); ?></a>
+	<?php endif; // phone. ?>
 			</div> <!-- .detail-properties -->
 		</div> <!-- .detail-subsection -->
 			<?php
@@ -114,7 +121,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div class="detail-image">
 			<?php
 
-			echo acf_esc_html( $image );
+			echo wp_kses_post( $image );
 
 			?>
 		</div>
