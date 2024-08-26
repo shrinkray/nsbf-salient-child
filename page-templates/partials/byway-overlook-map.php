3183e34725da42dd27ee36a96c048e67cd5ce254
Greg Miller <hello@gregmiller.io>
2024-08-26 16:20:11 -0400

    Update iframe version
    - Not working in local test
    - Likely being blocked by source

--- page-templates/partials/byway-overlook-map.php
+++ page-templates/partials/byway-overlook-map.php
@@ -14,28 +14,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 // vars.
 $national_overlook_map = the_field( 'national_map_url' );
 
-if ( $national_overlook_map ) :
-	?>
-<div class="grid grid-cols-1 mb-12 row md:grid-cols-2 lg:grid-cols-2">
+// Currently not working due to Content Security Policy or method to block what I wanted to do.
+?>
+<div class="row">
 
 	<div style="width: 100%; height: 500px; position: relative;">
-	<iframe src="<?php echo esc_url( $national_overlook_map ); ?>"
-			title="<?php echo esc_html( $official_byway_name ) . ' Map'; ?>"
-			aria-label="Interactive map showing byway route and location."
-			width="100%"
-			height="100%"
-			frameborder="0"
-			allowfullscreen
-			tabindex="0"
-			loading="lazy"
-			style="border: none; position: absolute; top: 0; left: 0;">
-	<!-- Fallback content -->
-		<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/svg/scenic-byway.svg"
-		alt="Your browser does not support iframes"
-		style="width: 100%; height: 100%; object-fit: contain;">
-	</iframe>
-</div>
+		<iframe src="<?php echo esc_html( $national_overlook_map ); ?>"
+				title="<?php echo esc_html( $official_byway_name ) . ' Map'; ?>"
+				aria-label="Interactive map showing byway route and location."
+				width="100%"
+				height="100%"
+				frameborder="0"
+				allowfullscreen
+				tabindex="0"
+				loading="lazy"
+				style="border: none; position: absolute; top: 0; left: 0;">
+		<!-- Fallback content -->
+			<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/svg/scenic-byway.svg"
+			alt="Your browser does not support iframes"
+			style="width: 100%; height: 100%; object-fit: contain;">
+		</iframe>
+	</div>
 
 </div> <!-- .row // Overlook map -->
 	<?php
-endif;
+
