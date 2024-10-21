9f6e0e86542c93e3000bcd759078fa3c18adebc8
Greg Miller <hello@gregmiller.io>
2024-10-21 13:16:01 -0400

    Update state-partners.php
    Refactor to display up to four columns of items.
    - Will look good with 2 columns of content
    - Did not test over 2 cols

--- page-templates/partials/state-partners.php
+++ page-templates/partials/state-partners.php
@@ -12,14 +12,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="spacer"></div>
-<div class="grid grid-cols-1 px-2 py-6 mt-8 detail-subsection md:grid-cols-2 border-goldenrod">
+<div class="grid grid-cols-1 gap-4 px-2 py-6 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:max-w-2xl lg:max-w-4xl xl:max-w-6xl detail-subsection border-goldenrod">
 	
-	<div class="p-2 text-center partner-digits">
+<?php 
+// vars.
+			$state_dot_name    			 = get_field( 'sp_state_department_of_transportation_name' );
+			$state_dot_website 			 = get_field( 'sp_state_department_of_transportation_website' );
+			$state_dot_phone   			 = get_field( 'sp_state_department_of_transportation_phone' );
+			$state_tourism_board_name    = get_field( 'sp_state_tourism_board_name' );
+			$state_tourism_board_website = get_field( 'sp_state_tourism_board_website' );
+			$state_tourism_board_phone   = get_field( 'sp_state_tourism_board_phone' );
+
+			/**
+			 * Uses Total cols are four. If calspan is 2, use full width 2-columns per item. 
+			 */
+
+			$columns = 0;
+
+			if ($state_dot_name) $columns++;
+			if ($state_tourism_board_name) $columns++;
+
+			$col_span = $columns == 1 ? 'md:col-span-2 lg:col-span-3 xl:col-span-4' : 'md:col-span-1 lg:col-span-2';
+   
+
+?>
+	<div class="p-2 text-center partner-digits <?php echo $col_span; ?>">
 		<?php
-			// vars.
-			$state_dot_name    = get_field( 'sp_state_department_of_transportation_name' );
-			$state_dot_website = get_field( 'sp_state_department_of_transportation_website' );
-			$state_dot_phone   = get_field( 'sp_state_department_of_transportation_phone' );
 
 			/**
 			 * If the organization exists, add it's name. If the web and or phone properties exist,
@@ -27,7 +45,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			 */
 		if ( $state_dot_name ) :
 			?>
-				<div class="text-xl font-bold detail-organization">
+				<div class="p-2 text-xl font-bold text-center detail-organization partner-digits ">
 					<?php echo esc_html( $state_dot_name ); ?></div>
 				
 				<div class="detail-properties">
@@ -50,36 +68,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 					// If we have a phone URL add a link.
 					if ( $state_dot_phone ) :
 						?>
+						<div class="p-2 text-center partner-digits <?php echo $col_span; ?>">
 							<a class="byway-phone-property" href="tel:
 							<?php
 							echo esc_html( $state_dot_phone );
 							?>
 							" title="Need help? Call our offices.">
 							<?php echo esc_html( $state_dot_phone ); ?></a>
+						</div>
 						<?php
+						
 					endif;
 					?>
 				</div> <!-- .detail-properties -->
 			<?php
 			endif; // $state_dot_name
-
+	
 		?>
 	</div>
-	<div class="px-2 pt-8 text-center partner-digits md:pt-2">
+	<div class="p-2 text-center partner-digits <?php echo $col_span; ?>">
 		<?php
-
-			// vars.
-			$state_tourism_board_name    = get_field( 'sp_state_tourism_board_name' );
-			$state_tourism_board_website = get_field( 'sp_state_tourism_board_website' );
-			$state_tourism_board_phone   = get_field( 'sp_state_tourism_board_phone' );
 
 			/**
 			 * If the organization exists, add it's name. If the web and or phone properties exist,
 			 * render, otherwise do not show them.
 			 */
+			
 		if ( $state_tourism_board_name ) :
 			// Removed the_content function.
 			?>
+			<div class="px-2 pt-8 text-center partner-digits md:pt-2 ">
 				<div class="text-xl font-bold detail-organization">
 					<?php echo esc_html( $state_tourism_board_name ); ?></div>
 				
@@ -113,11 +131,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 					endif;
 					?>
 				</div> <!-- .detail-properties -->
-	
+	</div>
 						<?php
 			endif; // $state_tourism_board_name.
 		?>
 	
 	</div>
+		
 
 </div> <!-- .detail-subsection // Statewide Byway Partners  -->
